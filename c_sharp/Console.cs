e5844c7677cffc35b8f4399caae188167a125cd5
Hoang Minh Vu <minhvu110200@gmail.com>
2026-08-14 18:32:11 +0700

    Add C# assignment (week 1)

---
+++ c_sharp/Console.cs
@@ -0,0 +1,11 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("hello world");
+        Console.WriteLine("anh do ximi");
+        Console.WriteLine("アナタ");
+        Console.WriteLine("um na na na");
+    }
+}
+
